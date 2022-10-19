9db8e29764135ffd3cd18a70ad669cdd5aee72c1
Roberto Paes <robertocpaes@gmail.com>
2022-10-18 21:18:40 -0300

    iniciando repositório

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { CpfComponent } from './documento/documento.component';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent implements AfterViewInit {
+
+  @ViewChild(CpfComponent) child: any;
+
+  constructor() { }
+
+  public handleClick: any;
+
+  ngAfterViewInit(): void {
+    this.handleClick = this.child.handleClick
+  }
+
+
+  title = 'Gerador CPFCNPJ';
 }
