c1380ee1a8d82096474337a0b3f4bdebaeca9b39
Robero Paes <robertocpaes@gmail.com>
2023-03-26 11:44:19 -0300

    adicionando variavel de ambietne fora do padrao do angular para testar o vercel

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: "" || process.env['baseUrl'],
-  apiKey: "" || process.env['apiKey']
+  baseUrl: process.env['baseUrl'],
+  apiKey: process.env['apiKey']
 };
